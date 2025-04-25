f5a1dc9004e561cf69f3350cfc48dc45c28d0faa
Diego Alonso Silva Bazan <N00351222@upn.pe>
2025-04-25 17:49:07 -0500

    Agregando los menus Agregar Alumno, Profeor y Notas

--- SistemadeMatricula/src/MenuPrincipal/Colegio.java
+++ SistemadeMatricula/src/MenuPrincipal/Colegio.java
@@ -5,11 +5,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class Colegio extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JMenuBar menuBar;
+	private JMenu mnNewMenu;
+	private JMenuItem mntmNewMenuItem;
+	private JMenuItem mntmNewMenuItem_1;
+	private JMenuItem mntmNewMenuItem_2;
 
 	/**
 	 * Launch the application.
@@ -34,11 +42,30 @@ public class Colegio extends JFrame {
 		setTitle("Menu Principal");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		{
+			menuBar = new JMenuBar();
+			setJMenuBar(menuBar);
+			{
+				mnNewMenu = new JMenu("Registros");
+				menuBar.add(mnNewMenu);
+				{
+					mntmNewMenuItem = new JMenuItem("Agregar Alumno");
+					mnNewMenu.add(mntmNewMenuItem);
+				}
+				{
+					mntmNewMenuItem_1 = new JMenuItem("Agregar Profesor");
+					mnNewMenu.add(mntmNewMenuItem_1);
+				}
+				{
+					mntmNewMenuItem_2 = new JMenuItem("Agregar Notas");
+					mnNewMenu.add(mntmNewMenuItem_2);
+				}
+			}
+		}
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 	}
-
 }
