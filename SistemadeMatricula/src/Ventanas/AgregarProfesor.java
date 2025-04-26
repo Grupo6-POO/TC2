5b7e71ef61ec244c58cb30a6903ff51798b0946d
Jhordy <Jhordy@192.168.1.14>
2025-04-26 10:23:00 -0500

    AGREGAR PROFESOR

--- SistemadeMatricula/src/Ventanas/AgregarProfesor.java
+++ SistemadeMatricula/src/Ventanas/AgregarProfesor.java
@@ -7,11 +7,15 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class AgregarProfesor extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
+	private JLabel lblNewLabel;
+	private JTextField TxtNombre;
 
 	/**
 	 * Launch the application.
@@ -32,9 +36,20 @@ public class AgregarProfesor extends JDialog {
 	public AgregarProfesor() {
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		{
+			lblNewLabel = new JLabel("Nombre");
+			lblNewLabel.setBounds(10, 10, 45, 13);
+			contentPanel.add(lblNewLabel);
+		}
+		{
+			TxtNombre = new JTextField();
+			TxtNombre.setBounds(101, 7, 96, 19);
+			contentPanel.add(TxtNombre);
+			TxtNombre.setColumns(10);
+		}
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -52,5 +67,4 @@ public class AgregarProfesor extends JDialog {
 			}
 		}
 	}
-
 }
